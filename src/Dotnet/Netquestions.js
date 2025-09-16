d8122cef8edb7c9a4a5cee7303a39cdb93bc4c1e
Anil <anilbca1996@gmail.com>
2025-09-16 10:58:48 +0530

    updated questions

--- src/Dotnet/Netquestions.js
+++ src/Dotnet/Netquestions.js
@@ -139,15 +139,18 @@ public class UserProfile
     public User User { get; set; }
 }
 
-public DbSet<User> Users { get; set; }
-public DbSet<UserProfile> UserProfiles { get; set; }
-
-protected override void OnModelCreating(ModelBuilder modelBuilder)
+public class AppDbContext : DbContext
 {
-    modelBuilder.Entity<User>()
-        .HasOne(u => u.UserProfile)
-        .WithOne(p => p.User)
-        .HasForeignKey<UserProfile>(p => p.UserId);
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserProfile> UserProfiles { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasOne(u => u.UserProfile)
+            .WithOne(p => p.User)
+            .HasForeignKey<UserProfile>(p => p.UserId);
+    }
 }`}
           </code>
         </pre>
