8c9e2488edd743b2beefb0d158b0c74cb9e41fbe
Anil <anilbca1996@gmail.com>
2026-06-20 11:26:54 +0530

    update intern

--- src/Components/SuperHeader.js
+++ src/Components/SuperHeader.js
@@ -237,7 +237,7 @@ useEffect(() => {
                   <p><strong>Work Mode:</strong> Work From Office</p>
                   <p><strong>Office Hours:</strong> 9:00 AM – 6:00 PM</p>
                   <p><strong>Locations:</strong> Hyderabad, Mohali & Dehradun</p>
-                  <p><strong>Stipend:</strong> 4k - 10k</p>
+                  <p><strong>Stipend:</strong> Stipend not available</p>
                   <p><strong>Placement:</strong> Direct join after completion the internship</p>
                 </div>
               </div>
